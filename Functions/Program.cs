f8c6be395f9d23482aca59b60442c9e981c0f306
Kaira Vain <kairavain@Kairas-MBP.lan>
2023-09-30 14:54:22 +0300

    add Functions code

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
 
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
